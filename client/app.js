95ed00e678a071ad62f3a208206b336e98c14269
Cliffe Hodgkinson <cliffe@resounding.ca>
2014-11-20 09:31:55 -0500

    Created VS project, added tsd

---
+++ client/app.js
@@ -0,0 +1,5 @@
+///<reference path="typings/jquery/jquery.d.ts" />
+///<reference path="typings/angularjs/angular.d.ts" />
+'use strict';
+angular.module('traxis', ['ngMaterial']);
+//# sourceMappingURL=app.js.map
