81a9f6151b019a1bd5d7169ae31a4da3bda33f0c
chodge <cliffe@resounding.ca>
2014-11-22 21:29:25 -0500

    Grunt & npm setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,37 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    'use strict';
 
-  // Project configuration.
-  grunt.initConfig({
-    typescript: {
-        
-    }
-  });
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  // Default task(s).
-  grunt.registerTask('default');
+    grunt.initConfig({
+        open: {
+        	path: 'http://localhost:3000'
+    	},
+        watch: {
+            options: {
+                livereload: true
+            },
+            all: {
+                files: ['app/**/*.js', 'app/**/*.html', 'public/**/*.*']
+            }
+        },
+        exec: {
+            server: {
+                cmd: 'node server.js'
+            }
+        },
+        touch: {
+        	src: ['public/index.html']
+    	}
+    });
 
+    grunt.registerTask('server', ['exec:server']);
+
+    grunt.registerTask('client', ['open', 'watch']);
+
+    grunt.registerTask('reload', ['touch']);
+
+    // define the default task that can be run just by typing "grunt" on the command line
+    // the array should contains the names of the tasks to run
+    grunt.registerTask('default', ['server', 'client']);
 };
